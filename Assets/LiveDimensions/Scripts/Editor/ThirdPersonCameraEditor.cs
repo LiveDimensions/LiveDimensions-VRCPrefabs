d004b176990bb4b480b1be51fe7b44930fa4d208
LiveDimensions <livedimensions@gmail.com>
2021-08-12 15:06:28 -0500

    ThirdPersonCamera v1.1

---
+++ Assets/LiveDimensions/Scripts/Editor/ThirdPersonCameraEditor.cs
@@ -0,0 +1,83 @@
+﻿
+#if !COMPILER_UDONSHARP && UNITY_EDITOR
+using UnityEditor;
+using UdonSharpEditor;
+using UnityEngine;
+#endif
+
+namespace LiveDimensions.ThirdPersonCamera
+{
+    #if !COMPILER_UDONSHARP && UNITY_EDITOR
+    [CustomEditor(typeof(ThirdPersonCamera))]
+    public class ThirdPersonCameraEditor : Editor
+    {
+        bool showReferences = false;
+
+        public override void OnInspectorGUI()
+        {
+            if (UdonSharpGUI.DrawDefaultUdonSharpBehaviourHeader(target)) return;
+            DrawInspectorTitle();
+
+            DrawKeyOptions();
+
+            DrawDebug();
+        }
+
+        private void DrawKeyOptions()
+        {
+            ThirdPersonCamera thirdPersonCamera = ((ThirdPersonCamera)target);
+
+            EditorGUI.BeginChangeCheck();
+
+            bool thirdPersonEnabled = EditorGUILayout.Toggle("Enable third person on start?", thirdPersonCamera.thirdPersonEnabled);
+
+            KeyCode enableThirdPersonKey = (KeyCode)EditorGUILayout.EnumPopup("Enable Third Person Key:", thirdPersonCamera.enableThirdPersonKey);
+            KeyCode frontViewKey = (KeyCode)EditorGUILayout.EnumPopup("Front View Key:", thirdPersonCamera.frontViewKey);
+            KeyCode backViewKey = (KeyCode)EditorGUILayout.EnumPopup("Back View Key:", thirdPersonCamera.backViewKey);
+            KeyCode leftShoulderKey = (KeyCode)EditorGUILayout.EnumPopup("Left Shoulder Key:", thirdPersonCamera.leftShoulderKey);
+            KeyCode rightShoulderKey = (KeyCode)EditorGUILayout.EnumPopup("Right Shoulder Key:", thirdPersonCamera.rightShoulderKey);
+
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(thirdPersonCamera, "Key option changed.");
+
+                thirdPersonCamera.thirdPersonEnabled = thirdPersonEnabled;
+
+                thirdPersonCamera.enableThirdPersonKey = enableThirdPersonKey;
+                thirdPersonCamera.frontViewKey = frontViewKey;
+                thirdPersonCamera.backViewKey = backViewKey;
+                thirdPersonCamera.leftShoulderKey = leftShoulderKey;
+                thirdPersonCamera.rightShoulderKey = rightShoulderKey;
+            }
+        }
+
+        private void DrawDebug()
+        {
+            GUILayout.Space(6);
+            showReferences = GUILayout.Toggle(showReferences, "(Debug) Show References", "Button");
+            if (showReferences) DrawDefaultInspector();
+        }
+
+        private void DrawInspectorTitle()
+        {
+            GUIStyle titleStyle = new GUIStyle();
+            titleStyle.fontSize = 16;
+            titleStyle.normal.textColor = EditorGUIUtility.isProSkin ? Color.white : Color.gray;
+
+            GUIStyle subtitleStyle = new GUIStyle();
+            subtitleStyle.fontSize = 12;
+            subtitleStyle.normal.textColor = EditorGUIUtility.isProSkin ? Color.gray : Color.gray;
+
+            GUILayout.Label("Third Person Camera", titleStyle);
+            GUILayout.Label("by LiveDimensions", subtitleStyle);
+
+            GUILayout.Space(6);
+
+            GUIStyle boxStyle = new GUIStyle();
+            boxStyle.normal.textColor = EditorGUIUtility.isProSkin ? Color.white : Color.black;
+            GUILayout.Box("This prefab should work out of the box, you can configure the keys used for changing between camera views.");
+        }
+    }
+    #endif
+}
---
+++ Assets/LiveDimensions/Scripts/ThirdPersonCamera/ThirdPersonCamera.cs
@@ -0,0 +1,152 @@
+﻿
+using Cinemachine;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace LiveDimensions.ThirdPersonCamera
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public class ThirdPersonCamera : UdonSharpBehaviour
+    {
+        //Hide in default inspector to show in custom inspector.
+        [HideInInspector] public bool thirdPersonEnabled = false;
+        [HideInInspector] public KeyCode enableThirdPersonKey = KeyCode.H;
+        [HideInInspector] public KeyCode leftShoulderKey = KeyCode.Q;
+        [HideInInspector] public KeyCode rightShoulderKey = KeyCode.E;
+        [HideInInspector] public KeyCode backViewKey = KeyCode.J;
+        [HideInInspector] public KeyCode frontViewKey = KeyCode.U;
+
+
+
+        //Show these in default inspector
+        [SerializeField] private Camera thirdPersonCamera;
+        [SerializeField] private Camera uiCamera;
+
+        [SerializeField] private Transform playerHead;
+
+        [SerializeField] private Transform cmTarget;
+        [SerializeField] private Transform cm180Target;
+
+        [SerializeField] private CinemachineVirtualCamera backCamera;
+
+        [SerializeField] private CinemachineVirtualCamera frontCamera;
+
+        [SerializeField] private CinemachineVirtualCamera leftShoulder;
+
+        [SerializeField] private CinemachineVirtualCamera rightShoulder;
+        
+
+        VRCPlayerApi localPlayer;
+        VRCPlayerApi.TrackingData headTrackingData;
+
+        private void Start()
+        {
+            localPlayer = Networking.LocalPlayer;
+
+            thirdPersonCamera.enabled = thirdPersonEnabled;
+            uiCamera.enabled = thirdPersonEnabled;
+
+            CalibrateThirdPersonCamera();
+        }
+
+        public override void PostLateUpdate()
+        {
+            if (!thirdPersonEnabled) return;
+
+            headTrackingData = localPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head);
+            playerHead.SetPositionAndRotation(headTrackingData.position, headTrackingData.rotation);
+        }
+
+        private void Update()
+        {
+
+            if (Input.GetKeyDown(enableThirdPersonKey))
+            {
+                thirdPersonEnabled = !thirdPersonEnabled;
+
+                thirdPersonCamera.enabled = thirdPersonEnabled;
+                uiCamera.enabled = thirdPersonEnabled;
+
+                if (thirdPersonEnabled)
+                {
+                    CalibrateThirdPersonCamera();
+                }
+
+            }
+
+            if (!thirdPersonEnabled) return;
+
+            if (frontCamera.enabled)
+            {
+                float scroll = Input.GetAxis("Mouse ScrollWheel");
+                if (scroll < 0 && cm180Target.localPosition.z < 50f)
+                {
+                    //Zoom out
+                    cm180Target.localPosition += new Vector3(0, 0, -scroll * 80f * Time.deltaTime);
+
+                } else if(scroll > 0 && cm180Target.localPosition.z > 0.1f)
+                {
+                    //Zoom in
+                    cm180Target.localPosition += new Vector3(0, 0, -scroll * 80f * Time.deltaTime);
+                }
+            }
+
+            //if(frontCamera.enabled) cm180Target.localPosition += (cm180Target.localPosition.z > -1.75f && cm180Target.localPosition.z < 50f)?new Vector3(0, 0, 80f * -Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime): Vector3.zero;
+
+            //Front
+            if (Input.GetKeyDown(frontViewKey))
+            {
+                backCamera.enabled = frontCamera.enabled;
+
+                rightShoulder.enabled = false;
+                leftShoulder.enabled = false;
+
+                frontCamera.enabled = !frontCamera.enabled;
+            }
+
+            //Back
+            if (Input.GetKeyDown(backViewKey))
+            {
+                backCamera.enabled = true;
+                frontCamera.enabled = false;
+                rightShoulder.enabled = false;
+                leftShoulder.enabled = false;
+            }
+
+            //Left Shoulder
+            if (Input.GetKeyDown(leftShoulderKey))
+            {
+                backCamera.enabled = leftShoulder.enabled;
+
+                rightShoulder.enabled = false;
+                frontCamera.enabled = false;
+
+                leftShoulder.enabled = !leftShoulder.enabled;
+            }
+
+            //Right Shoulder
+            if (Input.GetKeyDown(rightShoulderKey))
+            {
+                backCamera.enabled = rightShoulder.enabled;
+
+                leftShoulder.enabled = false;
+                frontCamera.enabled = false;
+
+                rightShoulder.enabled = !rightShoulder.enabled;
+            }
+
+        }
+
+        private void CalibrateThirdPersonCamera()
+        {
+            headTrackingData = localPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head);
+
+            cmTarget.localPosition = new Vector3(0, -(headTrackingData.position.y - localPlayer.GetBonePosition(HumanBodyBones.Neck).y), -(headTrackingData.position.y - localPlayer.GetPosition().y) * 0.5f);
+            cm180Target.localPosition = new Vector3(0, 0, (headTrackingData.position.y - localPlayer.GetPosition().y));
+        }
+    }
+
+}
+
